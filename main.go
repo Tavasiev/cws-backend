b9d628f782116da8726bcef32075ed1da041d283
Innonminate <ya.innonminate@yandex.ru>
2019-04-23 00:23:39 +0300

    Create handler CheckPhone

--- main.go
+++ main.go
@@ -34,6 +34,7 @@ func main() {
 	e.POST("/AddCity", handlers.AddCity)
 	e.POST("/AddWorker", handlers.AddWorker)
 	e.POST("/AddClient", handlers.AddClient)
+	e.POST("/CheckPhone", handlers.CheckPhone)
 
 	// Start server
 	e.Logger.Fatal(e.Start(configs.Cfg.Server.MainPort))
